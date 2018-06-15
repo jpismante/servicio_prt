01847e9967352081df54429dc0046d52e5e84281
José Pismante <jpismante@aceros-chile.local>
2018-06-15 10:30:04 -0400

    modificar conecta

--- conecta.php
+++ conecta.php
@@ -22,10 +22,11 @@
 			
 			
 		<?php
-			$host = "localhost";
-			$user ="root";
-			$pw = "";
-			$db ="servicio_prt";
+			$host = "23.21.238.28";
+			$user ="anjjmaptvflzwt";
+			$pw = "7c4a2861b9398fbd924df37ac4ccc75d173006388205912d74b6289a630422d6";
+			$db ="df4orf8aamnp52";
+			$port="5432";
 		?>
 
 ?>
